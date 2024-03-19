255a2de3c130048f17f64b7c77713b421ca12180
Sidney Aulakh <sidneyaulakh@gmail.com>
2024-03-19 18:41:21 +1100

    Initial Commit

---
+++ FearlessView/FearlessViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  FearlessViewApp.swift
+//  FearlessView
+//
+//  Created by Sidak Singh Aulakh on 19/3/2024.
+//
+
+import SwiftUI
+
+@main
+struct FearlessViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
